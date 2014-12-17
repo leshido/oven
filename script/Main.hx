86bf076637732e180b29f286efc72d75b3ef4dd0
Ido Leshem <leshido@gmail.com>
2014-12-18 00:37:01 +0200

    oven script first working

--- script/Main.hx
+++ script/Main.hx
@@ -1,7 +1,10 @@
 package;
 
+import haxe.io.Path;
+import haxe.Json;
 import neko.Lib;
 import sys.FileSystem;
+import sys.io.File;
 
 /**
  * ...
@@ -10,25 +13,82 @@ import sys.FileSystem;
 
 class Main 
 {
+	private static var _args:Array<String>;
+	private static var _config:Dynamic;
 	
 	static function main() 
 	{
-		// Set CWD
-		if (isCompilePending())
+		_args = Sys.args();
+
+		var jsonPath = getJsonFile();
+		var json = File.getContent(jsonPath);
+		_config = Json.parse(json);
+		
+		// Find plugin locations to add as -cp
+		var pluginDirs:Array<String> = [];
+		if (_config.config.pluginsDir != null)
 		{
-			// Compile
+			pluginDirs.push(_config.config.pluginsDir);
 		}
-		// Run oven.n
+		else
+		{
+			if (FileSystem.exists("plugins"))
+			{
+				pluginDirs.push("plugins");
+			}
+		}
+		var plugins:Array<Dynamic> = cast _config.plugins;
+		for (plugin in plugins)
+		{
+			if (plugin.path != null && pluginDirs.indexOf(plugin.path) == -1)
+			{
+				pluginDirs.push(plugin.path);
+			}
+		}
+		
+		// Build
+		var compilerArgs:Array<String> = [];
+		var outputFile:String = Path.join([Path.directory(jsonPath), "oven.n"]);
+		compilerArgs = compilerArgs.concat(["-cp", "src"]); //TODO: change to use -lib when used as haxelib
+		compilerArgs = compilerArgs.concat(["-main", "oven.Oven"]);
+		for (pluginDir in pluginDirs)
+		{
+			compilerArgs = compilerArgs.concat(["-cp", pluginDir]);
+		}
+		compilerArgs = compilerArgs.concat(["-resource", jsonPath + "@config"]);
+		compilerArgs = compilerArgs.concat(["-neko", outputFile]);
+		var buildStatus = Sys.command("haxe", compilerArgs);
+		
+		// Run
+		if (buildStatus == 0) Sys.command("neko " + outputFile);
 	}
 	
-	static private function isCompilePending():Bool
+	static function getJsonFile():String
 	{
-		if (!FileSystem.exists("oven.n"))
+		// Try to load Json file in order
+		var fileToLoad:String = "";
+		if (_args.length > 0 && _args[0].indexOf(".json") != -1)
 		{
-			return true;
+			fileToLoad = _args[0];
 		}
-		// Compare mtime of json file and oven.n
-		return true;
+		else
+		{
+			var possibleFiles:Array<String> = ["recipe.json", "oven.json", "project.json"];
+			for (file in possibleFiles)
+			{
+				if (FileSystem.exists(file))
+				{
+					fileToLoad = file;
+					break;
+				}
+			}
+		}
+		
+		if (fileToLoad == "")
+		{
+			throw "Could not find any JSON recipe file.";
+		}
+		return fileToLoad;
 	}
 	
 }
